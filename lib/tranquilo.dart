6f91ff026223cb0eb99e70d1e2e54e7578c33050
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-09-16 02:00:58 +0300

    edit main

--- lib/tranquilo.dart
+++ lib/tranquilo.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:tranquilo_app/core/routing/routes.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tranquilo_app/core/routing/app_router.dart';
+import 'package:tranquilo_app/core/theming/colors_manger.dart';
 
 class TranquiloApp extends StatelessWidget {
-  const TranquiloApp({super.key});
+  final AppRouter appRouter;
+
+  const TranquiloApp({super.key, required this.appRouter});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        title: 'Tranquilo App',
+        theme: ThemeData(
+          primaryColor: ColorsManager.oceanBlue,
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        debugShowCheckedModeBanner: false,
+        initialRoute: Routes.onBoardingScreen,
+        onGenerateRoute: appRouter.generateRoute,
+      ),
+    );
   }
 }
