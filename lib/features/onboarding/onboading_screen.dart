a91db593db829e13837c1d60c601979d96772d45
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-09-17 02:52:10 +0300

    add svgs to on boarding

---
+++ lib/features/onboarding/onboading_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class OnboadingScreen extends StatefulWidget {
+  const OnboadingScreen({super.key});
+
+  @override
+  State<OnboadingScreen> createState() => _OnboadingScreenState();
+}
+
+class _OnboadingScreenState extends State<OnboadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
