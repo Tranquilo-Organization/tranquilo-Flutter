863033a46ee5cf102d0ae8c72ef5bc6e445f19be
ibrahimagdy <iibbrraahhimm00@gmail.com>
2024-09-17 17:58:55 +0300

    Finish OTP design Successfully

---
+++ lib/features/otp/ui/otp_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tranquilo_app/features/otp/ui/widgets/otp_form.dart';
+import 'package:tranquilo_app/features/otp/ui/widgets/otp_header.dart';
+
+import '../../../core/helpers/spacing.dart';
+
+class OtpScreen extends StatelessWidget {
+  const OtpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              verticalSpace(16),
+              const OtpHeader(),
+              verticalSpace(64),
+              Image.asset(
+                'assets/images/logo.png',
+                height: 56.h,
+              ),
+              verticalSpace(64),
+              OtpForm(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
