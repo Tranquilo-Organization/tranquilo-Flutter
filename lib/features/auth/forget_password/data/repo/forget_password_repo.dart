61cf60fd872e3b4740da74c818688cd447cd3a51
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-09-18 02:38:52 +0300

    create the repo

--- lib/features/auth/forget_password/data/repo/forget_password_repo.dart
+++ lib/features/auth/forget_password/data/repo/forget_password_repo.dart
@@ -1,3 +1,21 @@
-class ForgetPasswordRepo{
+import 'package:tranquilo_app/core/network/api_result.dart';
+import 'package:tranquilo_app/core/network/api_service.dart';
+import 'package:tranquilo_app/core/network/api_error_handler.dart';
+import 'package:tranquilo_app/features/auth/forget_password/data/model/forget_password_request_model.dart';
+import 'package:tranquilo_app/features/auth/forget_password/data/model/forget_password_response_model.dart';
 
+class ForgetPasswordRepo {
+  final ApiService _apiService;
+
+  ForgetPasswordRepo(this._apiService);
+
+  Future<ApiResult<ForgetPasswordResponseModel>> forgetPassword(
+      ForgetPasswordRequestModel requestModel) async {
+    try {
+      final response = await _apiService.forgetPassword(requestModel);
+      return ApiResult.success(response);
+    } catch (error) {
+      return ApiResult.failure(ErrorHandler.handle(error));
+    }
+  }
 }
