170acb3b03e7ed43b7ed79f411644c7337158ba0
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-09-17 07:45:28 +0300

    create api service class

--- lib/core/network/api_service.dart
+++ lib/core/network/api_service.dart
@@ -1,3 +1,17 @@
-abstract class ApiService {
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:tranquilo_app/core/network/api_constants.dart';
+import 'package:tranquilo_app/features/auth/login/data/model/login_request_body.dart';
+import 'package:tranquilo_app/features/auth/login/data/model/login_response_body.dart';
 
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: ApiConstants.apiBaseUrl)
+abstract class ApiService {
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @POST(ApiConstants.login)
+  Future<LoginResponseBody> login(
+    @Body() LoginRequestBody loginRequestBody,
+  );
 }
