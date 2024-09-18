27da3ae6911a45329fb4a4393109f66cf316529d
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-09-18 04:50:21 +0300

    create repo

--- lib/core/network/api_service.dart
+++ lib/core/network/api_service.dart
@@ -7,7 +7,9 @@ import 'package:tranquilo_app/features/auth/login/data/model/login_response_body
 import 'package:tranquilo_app/features/auth/sign_up/data/model/sign_up_request_body.dart';
 import 'package:tranquilo_app/features/auth/otp/data/models/verify_otp_request_model.dart';
 import 'package:tranquilo_app/features/auth/otp/data/models/verify_otp_response_model.dart';
+import 'package:tranquilo_app/features/auth/reset_password/data/models/reset_password_request_model.dart';
 import 'package:tranquilo_app/features/auth/forget_password/data/model/forget_password_request_model.dart';
+import 'package:tranquilo_app/features/auth/reset_password/data/models/reset_password_response_model.dart';
 import 'package:tranquilo_app/features/auth/forget_password/data/model/forget_password_response_model.dart';
 
 part 'api_service.g.dart';
@@ -34,5 +36,10 @@ abstract class ApiService {
   @POST(ApiConstants.verifyOtp)
   Future<VerifyOtpResponseModel> verifyOtp(
     @Body() VerifyOtpRequestModel verifyOtpRequestModel,
+  );
+
+  @POST(ApiConstants.resetPassword)
+  Future<ResetPasswordResponseModel> resetPassword(
+    @Body() ResetPasswordRequestModel resetPasswordRequestModel,
   );
 }
--- lib/core/network/api_service.g.dart
+++ lib/core/network/api_service.g.dart
@@ -161,6 +161,41 @@ class _ApiService implements ApiService {
     return _value;
   }
 
+  @override
+  Future<ResetPasswordResponseModel> resetPassword(
+      ResetPasswordRequestModel resetPasswordRequestModel) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(resetPasswordRequestModel.toJson());
+    final _options = _setStreamType<ResetPasswordResponseModel>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          'account/reset-password',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late ResetPasswordResponseModel _value;
+    try {
+      _value = ResetPasswordResponseModel.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
