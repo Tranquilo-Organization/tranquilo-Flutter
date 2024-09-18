186f4e99f3f0df5b127d4018cb88fda8de221d42
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-09-18 09:53:10 +0300

    starting survey design

--- lib/features/survey/ui/screens/survey_starting.dart
+++ lib/features/survey/ui/screens/survey_starting.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:tranquilo_app/core/theming/styles.dart';
 import 'package:tranquilo_app/core/helpers/spacing.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tranquilo_app/core/widgets/app_text_button.dart';
 
 class SurveyStarting extends StatelessWidget {
   const SurveyStarting({super.key});
@@ -11,16 +12,49 @@ class SurveyStarting extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: 24.w, vertical: 24.h), // Increased padding
           child: Column(
-        children: [
-          Text(
-            'Let’s start GAD survey',
-            style: TextStyles.font20JetBlackSemiBold,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              verticalSpace(20), // Space added to push content down a bit
+              Text(
+                'Let’s start GAD survey',
+                style: TextStyles.font20JetBlackSemiBold,
+              ),
+              verticalSpace(3), // Reduced space between text and image
+              Expanded(
+                child: SvgPicture.asset(
+                  'assets/svgs/survey_start.svg',
+                  width: 250.w, // Adjusted size of the image
+                  fit: BoxFit.contain,
+                ),
+              ),
+              verticalSpace(20),
+              Text(
+                'This questionnaire will help us understand your anxiety symptoms and provide personalized recommendations for managing them.',
+                textAlign: TextAlign.center,
+                style: TextStyles.font14JetBlackMedium,
+              ),
+              verticalSpace(30),
+              AppTextButton(
+                textButton: 'Start',
+                onPressed: () {
+                  // Handle the start button press
+                },
+              ),
+              verticalSpace(20),
+              Text(
+                "By clicking 'Start' you agree to our Terms of Service & Privacy Policy.",
+                textAlign: TextAlign.center,
+                style: TextStyles.font12LightSilverMedium,
+              ),
+              verticalSpace(10), // Small space at the bottom
+            ],
           ),
-          verticalSpace(30),
-          SvgPicture.asset('assets/svgs/survey_start.svg')
-        ],
-      )),
+        ),
+      ),
     );
   }
 }
