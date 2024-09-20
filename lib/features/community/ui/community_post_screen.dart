c1a86d019b2a8cd4b706ca54125376d6ab31e3cd
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-09-20 03:43:21 +0300

    fix bugs

---
+++ lib/features/community/ui/community_post_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CommunityPostScreen extends StatefulWidget {
+  const CommunityPostScreen({super.key});
+
+  @override
+  State<CommunityPostScreen> createState() => _CommunityPostScreenState();
+}
+
+class _CommunityPostScreenState extends State<CommunityPostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
