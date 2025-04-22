1b591f233fd9d0b91cf349f24dba1be365b23c52
ibrahimagdy <iibbrraahhimm00@gmail.com>
2025-04-22 20:50:15 +0200

    Edit ios config

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,7 @@ import file_selector_macos
 import flutter_secure_storage_macos
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
